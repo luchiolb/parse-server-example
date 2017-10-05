6274776e54e846c87c8cbee60f9620abc05d98c4
Luciano Simo <lucianosimo87@gmail.com>
2017-10-05 14:09:43 -0300

    Fix for updating records from web

--- cloud/main.js
+++ cloud/main.js
@@ -3,9 +3,6 @@ Parse.Cloud.beforeSave('Record', function(request, response) {
     var record = Parse.Object.extend('Record');
     var query = new Parse.Query(record);
   
-    //Setting seconds to 0
-    //request.object.get('checkInDate').setSeconds(0); 
-  
     //When Saving checkIn
     if (request.object.get('checkOutDate') == null) {
         //Check if record already exists without a Check-out
@@ -39,14 +36,20 @@ Parse.Cloud.beforeSave('Record', function(request, response) {
         response.success();
     } //When Saving CheckOut 
     else {
-        var checkOutDate = new Date();
+        var checkOutDate = '';
+
+        //Modifying record from Web
+        if (request.object.get('wasModified')) {
+            checkOutDate = request.object.get('checkOutDate');
+        } else { 
+            //New checkout date
+            checkOutDate = new Date();
+        }
 
         //Set CheckIn Seconds to 0
-        //request.object.get('checkOutDate').setSeconds(0);
         checkOutDate.setSeconds(0);
   
         //Get the difference between checkIn and CheckOut. Rounded to 2 decimals.
-        //var hours = Math.abs(request.object.get('checkOutDate') - request.object.get('checkInDate')) / 36e5;
         var hours = Math.abs(checkOutDate - request.object.get('checkInDate')) / 36e5;
 
         request.object.set("workedHours", hours.round(2));
